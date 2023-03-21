d13c07a6e6a7ae18ed4e0a454fb320b680fe2c34
Thaddaeus <thaddaeusotieno@gmail.com>
2023-03-21 12:39:32 +0300

    C:/Program Files/Git/

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {GoogleOutlined, FacebookOutline} from '@ant-design/icons'
+const Login = () => {
+    return(
+        <div id= 'login_page'>
+            Login
+        </div>
+    )
+}
+
+export default Login
